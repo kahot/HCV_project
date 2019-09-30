7fe8fba2c03e7ffb3480473eca2cb5487c6d5d0c
Kaho Tisthammer <ktisthammer@gmail.com>
2019-09-30 13:03:13 -0700

    script updates

--- Rscripts/30.3.Volcano.plots2.R
+++ Rscripts/30.3.Volcano.plots2.R
@@ -4,7 +4,6 @@ library(zoo)
 library(reshape2)
 library(ggplot2)
 library(DescTools)
-library(reshape2)
 library(colorspace)
 source("Rscripts/baseRscript.R")
 
@@ -138,97 +137,3 @@ for (g in 1:2){
 }
 
 
-###
-topFst<-list()
-fsts<-list.files("Output_all/Fst_T/", pattern=glob2rx("*^HighFst*.csv*"))
-for (i in 1:3){
-        topFst[[i]]<-read.csv(paste0("Output_all/Fst_T/",fsts[i]),stringsAsFactors = F, row.names = 1)
-        names(topFst)[i]<-substr(fsts[i], start=14, stop = 18)
-}
-summaryFst<-list()
-fsts2<-list.files("Output_all/Fst_T/", pattern=glob2rx("*^Summary.Fst*.csv*"))
-for (i in 1:3){
-        summaryFst[[i]]<-read.csv(paste0("Output_all/Fst_T/",fsts2[i]),stringsAsFactors = F)
-        names(summaryFst)[i]<-substr(fsts[i], start=14, stop = 18)
-}
-
-Summary<-read.csv("Output_all/Unfiltered/Ts.MinorVariant_Mean_3genotypes.csv",stringsAsFactors = F, row.names = 1)
-Summary$gene[Summary$gene=="NS1(P7)"]<-"NS1"
-Summary<-Summary[Summary$merged.pos>263,]
-
-#Separate the sites that are 
-
-#Summary<-read.csv("Output_all/Unfiltered/Ts.Same_Mean_3genotypes.csv",stringsAsFactors = F, row.names = 1)
-#Summary$gene[Summary$gene=="NS1(P7)"]<-"NS1"
-
-
-
-# Plot all frequencies with ref1a!=ref3a
-for (g in 2:3){  
-        dt<-summaryFst[[g]]
-        dt.top<-topFst[[g]]
-        fname<-names(FstList)[g]
-        g1<-substr(fname, start=5, stop = 6)
-        g2<-substr(fname, start=8, stop = 9)
-        
-        if (g==1) {col1<-colors2[1]; col2<-colors2[3]}
-        if (g==2) {col1<-colors2[1]; col2<-colors2[5]}
-        if (g==3) {col1<-colors2[3]; col2<-colors2[5]}
-        
-        pdf(paste0("Output_all/Fst_T/HighFst_positions",fname,  ".byGenes.pdf"), width=8, height = 18)
-        par(mfrow = c(6,2))
-        for (i in 1:11){
-                tb1<-dt.top[dt.top$gene==genes$Gene[i+1]&dt.top$diff<0,]
-                tb1<-tb1[!is.na(tb1$diff),]
-                tb2<-dt[dt.top$gene==genes$Gene[i+1]&dt.top$diff>0,]
-                tb2<-tb2[!is.na(tb2$diff),]
-                
-                dt1<-dt[dt$gene==genes$Gene[i+1],]
-                plot(dt1$Fst~dt1$merged.pos, pch=16, col="gray70", ylim=c(-0.01,0.08),  main =genes$Gene[i+1],cex=0.7, xlab="Genome position", ylab="Fst")
-                points(tb1$Fst~tb1$merged.pos,ylim=c(0,0.08), pch=16, col=col1)
-                points(tb2$mean.1B~tb2$merged.pos,ylim=c(0,0.08), pch=16, col=col2)
-                legend("topright", col=c(col1,col2), legend=c(g1,g2), pch=16, cex=.8, bty="n")
-        }
-        dev.off()
-        
-        pdf(paste0("Output_all/Fst_T/HighFst_MF_positions",fname,  "_genes.pdf"), width=8, height = 18)
-        par(mfrow = c(6,2))
-        for (i in 1:11){
-                tb1<-dt.top[dt.top$gene==genes$Gene[i+1]&dt.top$diff<0,]
-                tb1<-tb1[!is.na(tb1$diff),]
-                tb2<-dt.top[dt.top$gene==genes$Gene[i+1]&dt.top$diff>0,]
-                tb2<-tb2[!is.na(tb2$diff),]
-                
-                dt1<-dt[dt$gene==genes$Gene[i+1],]
-                plot(dt1[,paste0("mean.",g1)]~dt1$merged.pos, pch=16, col="gray70",  main =paste(genes$Gene[i+1], g1), ylim=c(0,0.08), cex=0.7,xlab="Genome position", ylab="Mutation frequency")
-                points(tb1[,paste0("mean.",g1)]~tb1$merged.pos,ylim=c(0,0.08), pch=16, col=col1)
-                
-                plot(dt1[,paste0("mean.",g2)]~dt1$merged.pos, pch=16, main =paste(genes$Gene[i+1], g2), col="gray70", ylim=c(0,0.08), cex=0.7,xlab="Genome position", ylab="Mutation frequency")
-                points(tb2[,paste0("mean.",g2)]~tb2$merged.pos,ylim=c(0,0.08), pch=16, col=col2)
-                
-        }
-       dev.off() 
-}
-
-# top 5%
-dt.top<-topFst[[1]]
-dt<-summaryFst[[1]]
-
-i=3
-
-pdf(paste0("Output_all/Fst_T/HighFst_positions",fname,  "_genes.pdf"), width=8, height = 18)
-par(mfrow = c(6,2))
-for (i in 1:11){
-        tb1<-dt.top[dt.top$gene==genes$Gene[i+1]&dt.top$diff<0,]
-        tb1<-tb1[!is.na(tb1$diff),]
-        tb2<-dt[dt.top$gene==genes$Gene[i+1]&dt.top$diff>0,]
-        tb2<-tb2[!is.na(tb2$diff),]
-        
-        dt1<-dt[dt$gene==genes$Gene[i+1],]
-        plot(dt1$mean.1A~dt1$merged.pos, pch=16, col="gray70", ylim=c(0,0.08), cex=0.7)
-        points(tb1$mean.1A~tb1$merged.pos,ylim=c(0,0.08), pch=16, col=colors2[1])
-        
-        plot(dt1$mean.1B~dt1$merged.pos, pch=16, col="gray70", ylim=c(0,0.08), cex=0.7)
-        points(tb2$mean.1B~tb2$merged.pos,ylim=c(0,0.08), pch=16, col=colors2[1])
-        
-        
